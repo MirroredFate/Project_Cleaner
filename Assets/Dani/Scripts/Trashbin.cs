f1bfe45f3af0350dd4a7a532441b47e896781897
MirroredFate <34125179+MirroredFate@users.noreply.github.com>
2018-06-26 12:07:00 +0200

    added hit detection

---
+++ Assets/Dani/Scripts/Trashbin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashbin : MonoBehaviour {
+
+    public Transform target;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+
+        Debug.DrawLine(transform.position, target.position, Color.red);
+
+        if (Physics.Linecast(transform.position, target.position, out hit))
+        {
+            Debug.Log("blocked by: " + hit.transform.gameObject.name);
+        }
+    }
+}
