865dd58796da9edc8b5d0a225e034ae011a2e380
MirroredFate <34125179+MirroredFate@users.noreply.github.com>
2018-06-26 12:17:14 +0200

    score added

--- Assets/Dani/Scripts/Trashbin.cs
+++ Assets/Dani/Scripts/Trashbin.cs
@@ -6,7 +6,9 @@ public class Trashbin : MonoBehaviour {
 
     public Transform target;
 
-    
+    public ScoreManager scoreManager;
+
+    bool clicked = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +18,17 @@ public class Trashbin : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicked = true;
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            clicked = false;
+        }
+
+
         RaycastHit hit;
 
         Debug.DrawLine(transform.position, target.position, Color.red);
@@ -23,7 +36,11 @@ public class Trashbin : MonoBehaviour {
         if (Physics.Linecast(transform.position, target.position, out hit))
         {
             Debug.Log("blocked by: " + hit.transform.gameObject.name);
-
+            if(clicked == false)
+            {
+                Destroy(hit.transform.gameObject);
+                scoreManager.score += 100;
+            }
 
         }
     }
