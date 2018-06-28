1f61d5feab8e6f8a8387278b10a58cc31440848b
MirroredFate <Karl_daniel@hotmail.de>
2018-06-28 12:47:22 +0200

    WIP Cleaning

---
+++ Assets/Dani/Scripts/Cleaning.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleaning : MonoBehaviour {
+
+    [Range(0.1f, 1f)]
+    public float cleaningSpeed = 0.5f;
+
+    float alpha = 255f;
+
+    bool isDragging;
+    bool foundDirt;
+
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+
+    private void Update()
+    {
+        Clean();
+    }
+
+
+    private void Clean(Renderer rend)
+    {
+        Color color = rend.material.color;
+        color.a -= Time.deltaTime * cleaningSpeed;
+        rend.material.color = color;
+    }
+}
