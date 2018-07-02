863262c4f1096ff0716d819a958ccce137afd1f1
MirroredFate <Karl_daniel@hotmail.de>
2018-07-02 15:36:33 +0200

    adjusted stuff to fit the phone screen

--- Assets/Dani/Scripts/Sponge.cs
+++ Assets/Dani/Scripts/Sponge.cs
@@ -22,7 +22,7 @@ public class Sponge : MonoBehaviour {
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
